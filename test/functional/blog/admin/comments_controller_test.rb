ae39f939f85e3ebaf317e43af87f6d2d19b203fb
Oleg <oleg@theworkinggroup.ca>
2012-01-18 16:39:41 -0500

    admin comments are ok

--- test/functional/blog/admin/comments_controller_test.rb
+++ test/functional/blog/admin/comments_controller_test.rb
@@ -17,5 +17,22 @@ class Blog::Admin::CommentsControllerTest < ActionController::TestCase
     assert assigns(:post)
     assert assigns(:comments)
   end
+  
+  def test_publish
+    comment = blog_comments(:default)
+    comment.update_attribute(:is_published, false)
+    xhr :put, :publish, :id => comment
+    assert_response :success
+    
+    comment.reload
+    assert comment.is_published
+  end
+  
+  def test_destroy
+    assert_difference 'Blog::Comment.count', -1 do
+      xhr :delete, :destroy, :id => blog_comments(:default)
+      assert_response :success
+    end
+  end
 
 end
