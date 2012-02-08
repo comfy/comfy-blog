b95296ee807976c0ca4372d3322ccaf07de20e5e
Oleg <oleg@theworkinggroup.ca>
2012-02-08 16:17:34 -0500

    fixing path helper

--- app/helpers/blog/application_helper.rb
+++ app/helpers/blog/application_helper.rb
@@ -12,8 +12,12 @@ module Blog::ApplicationHelper
   # URL helpers for blog_post_(path/url)
   %w(path url).each do |type|
     define_method "blog_post_#{type}" do |post|
-      send("dated_blog_post_#{type}", post.year, post.month, post.slug)
+      send("dated_blog_post_#{type}", post.year, ("%02d" % post.month), post.slug)
     end
+  end
+  
+  def blah
+    'blah'
   end
   
 end
