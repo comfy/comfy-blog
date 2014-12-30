3895cef3627af4255e27120ba602ffaa72874051
piotr <piotr@bitsilk.com>
2014-12-30 14:20:09 +0100

    Modified configuration file

--- config/initializers/comfy_blog.rb
+++ config/initializers/comfy_blog.rb
@@ -3,6 +3,10 @@ ComfyBlog.configure do |config|
   # Number of posts per page. Default is 10
   #   config.posts_per_page = 10
 
+  # Enable/disable comments on posts
+  # Default is true
+  #   config.allow_comments = false
+
   # Comments can be automatically approved/published by changing this setting
   # Default is false.
   #   config.auto_publish_comments = false
