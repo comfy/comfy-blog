21917f0d71af0ddcc16259279964d66678d2e410
Oleg <oleg@khabarov.ca>
2015-01-25 10:03:54 -0500

    using pagination concern from cms

--- app/controllers/comfy/blog/posts_controller.rb
+++ app/controllers/comfy/blog/posts_controller.rb
@@ -1,17 +1,17 @@
 class Comfy::Blog::PostsController < Comfy::Blog::BaseController
-  
+
   skip_before_action :load_blog, :only => [:serve]
-  
+
   # due to fancy routing it's hard to say if we need show or index
   # action. let's figure it out here.
   def serve
     # if there are more than one blog, blog_path is expected
-    if @cms_site.blogs.count >= 2 
+    if @cms_site.blogs.count >= 2
       params[:blog_path] = params.delete(:slug) if params[:blog_path].blank?
     end
-    
+
     load_blog
-    
+
     if params[:slug].present?
       show && render(:show)
     else
@@ -30,14 +30,14 @@ class Comfy::Blog::PostsController < Comfy::Blog::BaseController
     limit = ComfyBlog.config.posts_per_page
     respond_to do |format|
       format.html do
-        @posts = scope.page(params[:page]).per(limit)
+        @posts = comfy_paginate(scope, limit)
       end
       format.rss do
         @posts = scope.limit(limit)
       end
     end
   end
-  
+
   def show
     @post = if params[:slug] && params[:year] && params[:month]
       @blog.posts.published.where(:year => params[:year], :month => params[:month], :slug => params[:slug]).first!
