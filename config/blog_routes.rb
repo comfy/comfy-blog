925d293d3a746997eaa91038a3b4c904c576d0c9
Natasha Jones <tasha.k.jones@gmail.com>
2015-02-13 15:15:02 -0500

    add tag route

--- config/blog_routes.rb
+++ config/blog_routes.rb
@@ -3,4 +3,5 @@ ComfyBlog::Application.routes.draw do
   comfy_route :blog_admin
   comfy_route :blog
   comfy_route :cms, :sitemap => true
+  comfy_route :tags, :path => 'articles#index', as: :tag
 end
