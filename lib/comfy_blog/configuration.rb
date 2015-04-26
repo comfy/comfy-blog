43fd2db3bad91f99387a3b41d0853f4c8e8b596c
Jack Weeden <jack@ajack.org>
2015-04-26 19:07:32 +0100

    Allow action to be configured when rescuing from ActiveRecord::RecordNotFound

--- lib/comfy_blog/configuration.rb
+++ lib/comfy_blog/configuration.rb
@@ -7,15 +7,19 @@ module ComfyBlog
     # Comments can be automatically approved/published by changing this setting
     # Default is false.
     attr_accessor :auto_publish_comments
-    
+
     # A default author can be specified for posts
     attr_accessor :default_author
+
+    # A proc to be called when a post cannot be found
+    attr_accessor :post_not_found
 
     # Configuration defaults
     def initialize
       @posts_per_page         = 10
       @auto_publish_comments  = false
       @default_author         = nil
+      @post_not_found = Proc.new { render :cms_page => '/404', :status => 404 }
     end
 
   end
