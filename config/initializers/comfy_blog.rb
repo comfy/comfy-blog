1b43db64fbf76fad7cf60b7ce91aaaac0b4f2d95
Oleg <oleg@khabarov.ca>
2017-11-28 23:48:31 -0800

    adjusting initializer

--- config/initializers/comfy_blog.rb
+++ config/initializers/comfy_blog.rb
@@ -1,5 +1,8 @@
 ComfyBlog.configure do |config|
 
+  # application layout to be used to index blog posts
+  #   config.app_layout = 'comfy/blog/application'
+
   # Number of posts per page. Default is 10
   #   config.posts_per_page = 10
 
