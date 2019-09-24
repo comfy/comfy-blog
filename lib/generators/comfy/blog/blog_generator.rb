f3fc056759841ecb7962f19b003a04a21c12455a
Ryan Richardson <ryananthonyrichardson@gmail.com>
2019-09-24 15:38:51 -0700

    Add image display to blog index

--- lib/generators/comfy/blog/blog_generator.rb
+++ lib/generators/comfy/blog/blog_generator.rb
@@ -32,9 +32,7 @@ module Comfy
           )
 
           if destination
-            puts "\e[0m\e[31mFound existing #{identifier
-              .gsub(version_regex, '')} migration. Remove it \
-              if you want to regenerate.\e[0m"
+            puts "\e[0m\e[31mFound existing #{identifier.gsub(version_regex, '')} migration. Remove it if you want to regenerate.\e[0m"
           else
             migration_template "db/migrate/#{identifier}.rb",
               "db/migrate/#{identifier.gsub(version_regex, '')}.rb"
